ad2bc2b575f24afb483a63874638c452a12638d2
Olivia Colomier <51862620+oliviacolomier@users.noreply.github.com>
2021-03-14 16:07:14 -0400

    error handling added

--- WomxnDevelopUbisoftDemo/TextureHolder.cpp
+++ WomxnDevelopUbisoftDemo/TextureHolder.cpp
@@ -6,15 +6,21 @@
 void TextureHolder::load(Textures::ID id, const std::string& filename)
 {
 	std::unique_ptr<sf::Texture> texture(new sf::Texture());
-	texture->loadFromFile(filename);
+	if (!texture->loadFromFile(filename))
+		throw std::runtime_error("TextureHolder::load- Failed to load" + filename);
 
-	mTextureMap.insert(std::make_pair(id, std::move(texture)));
+	auto inserted = mTextureMap.insert(std::make_pair(id, std::move(texture)));
+	assert(inserted.second);
+	
 
 }
 sf::Texture& TextureHolder::get(Textures::ID id)
 {
 	auto found = mTextureMap.find(id);
+	assert(found != mTextureMap.end());
+
 	return *found->second;
+
 }
 
 const sf::Texture& TextureHolder::get(Textures::ID id) const
